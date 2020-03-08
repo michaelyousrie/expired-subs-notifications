9e44ee40deeaac6722bd1e12b3296ed2e9b0724c
Michael <michaelyousrie@gmail.com>
2020-03-08 03:16:31 +0200

    add client model, migration and seeder.

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Client;
+use Faker\Generator as Faker;
+
+$factory->define(Client::class, function (Faker $faker) {
+    return [
+        'name'          => $faker->word,
+        'join_date'     => now(),
+        'sub_end_date'  => now()->addYear(),
+        'bundle_name'   => $faker->word
+    ];
+});
